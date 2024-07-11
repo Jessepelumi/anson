05167c325a9aff708f3c0e7172b82fdc17a67181
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-07-11 23:55:54 +0100

    added products list

---
+++ src/static/products.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    name: "Pixel 7 Pro",
+    brand: "Google",
+    price: 700,
+  },
+  {
+    id: 2,
+    name: "iPhone 13",
+    brand: "Apple",
+    price: 700,
+  },
+  {
+    id: 3,
+    name: "iPhone 15 Pro",
+    brand: "Apple",
+    price: 900,
+  },
+  {
+    id: 4,
+    name: "Galaxy S24 Ultra",
+    brand: "Samsung",
+    price: 1000,
+  },
+  {
+    id: 5,
+    name: "Fold 5",
+    brand: "Samsung",
+    price: 950,
+  },
+  {
+    id: 6,
+    name: "OnePlus 10 Pro",
+    brand: "OnePlus",
+    price: 950,
+  },
+  {
+    id: 7,
+    name: "Galaxy S21",
+    brand: "Samsung",
+    price: 650,
+  },
+  {
+    id: 8,
+    name: "iPhone 14 Plus",
+    brand: "Apple",
+    price: 750,
+  },
+];

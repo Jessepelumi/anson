bdc45143140aacb5f0f1bfb343e82f4df1b25247
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-07-12 16:57:46 +0100

    added authentication api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import usersRouter from "./users_api.js";
 import productsRouter from "./products_api.js";
+import authRouter from "./auth_api.js";
 
 const router = Router();
 
 router.use(usersRouter);
 router.use(productsRouter);
+router.use(authRouter);
 
 export default router;

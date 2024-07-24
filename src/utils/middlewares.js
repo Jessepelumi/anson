41f1419d8dbec3823170c8b0630cf54d38996d0d
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-07-24 12:43:01 +0100

    imported mongodm schema

--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -1,15 +1,17 @@
-import { users } from "../static/users.js";
+//import { users } from "../static/users.js";
+import { User } from "../mongoose/schema/users.js";
 
 export const resolveIndexByUserId = (req, res, next) => {
   const {
-    params: { id },
+    params: { username },
   } = req;
 
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) return res.sendStatus(400);
+  // const parsedId = parseInt(id);
+  // if (isNaN(parsedId)) return res.sendStatus(400);
 
-  const findUserIndex = users.findIndex((user) => user.id === parsedId);
-  if (findUserIndex === -1) return res.sendStatus(404);
-  req.findUserIndex = findUserIndex;
+  //const findUserIndex = users.findIndex((user) => user.id === parsedId);
+  const findUser = User.findOne(username);
+  if (!findUser) return res.sendStatus(404);
+  req.findUser = findUser;
   next();
 };

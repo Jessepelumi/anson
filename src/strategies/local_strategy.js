310bb34879a0116a297fbd5683845814033666d2
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-07-24 12:41:53 +0100

    passport local authentication

---
+++ src/strategies/local_strategy.js
@@ -0,0 +1,32 @@
+import passport from "passport";
+import { Strategy } from "passport-local";
+import { User } from "../mongoose/schema/users.js";
+import { comparePassword } from "../utils/helpers.js";
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const findUser = await User.findById(id);
+    if (!findUser) throw new Error("User not found");
+    done(null, findUser);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+export default passport.use(
+  new Strategy(async (username, password, done) => {
+    try {
+      const findUser = await User.findOne({ username });
+      if (!findUser) throw new Error("User not found");
+      if (!comparePassword(password, findUser.password))
+        throw new Error("Invalid password");
+      done(null, findUser);
+    } catch (error) {
+      done(error, null);
+    }
+  })
+);

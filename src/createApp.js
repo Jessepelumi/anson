2adc54a714dbcd287c3b42926a7ecf259bb99f32
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-07-26 08:37:28 +0100

    moved app instance logic to a separate file

---
+++ src/createApp.js
@@ -0,0 +1,32 @@
+import express from "express";
+import routes from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
+import MongoStore from "connect-mongo";
+
+export function createApp() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: "jeolad-anson-express-tutorial",
+      saveUninitialized: true,
+      resave: false,
+      cookie: {
+        maxAge: 60000 * 60 * 24 * 7,
+      },
+      store: MongoStore.create({
+        client: mongoose.connection.getClient(),
+      }),
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(routes);
+
+  return app;
+}

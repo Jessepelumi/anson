4a41377e74e529805591f061e16cbc24644b5e89
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-07-27 07:39:12 +0100

    cleaned up main file

--- src/server.js
+++ src/server.js
@@ -1,37 +1,12 @@
-import express from "express";
-import routes from "./routes/index.js";
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import passport from "passport";
+import { createApp } from "./createApp.js";
 import mongoose from "mongoose";
-import MongoStore from "connect-mongo";
-
-const app = express();
 
 mongoose
   .connect("mongodb://localhost/anson")
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log(`Error: ${error}`));
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  session({
-    secret: "jeolad-anson-express-tutorial",
-    saveUninitialized: true,
-    resave: false,
-    cookie: {
-      maxAge: 60000 * 60 * 24 * 7,
-    },
-    store: MongoStore.create({
-      client: mongoose.connection.getClient(),
-    }),
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(routes);
+const app = createApp();
 
 const PORT = process.env.PORT || 3000;
 
@@ -46,3 +21,6 @@ app.listen(PORT, () => {
 //   res.cookie("hello", "world", { maxAge: 60000 * 60 * 24, signed: true });
 //   res.status(201).send("Welcome to Express.js server");
 // });
+
+// client secret - qLYEOJGVkzIiM5jNbgOgKtCxpiE2TplI
+// client id - 1265638466423881905
